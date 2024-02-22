9c42fc5dd03f16f1ea30423a03f9ae88528dce98
jforgue <26842354+bandogora@users.noreply.github.com>
2024-02-22 12:07:13 -0500

    Fix submodule init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 
-git -C $dir submodule init
+git -C $dir submodule init && git -C $dir submodule update
 git -C $dir submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
 
 (cd $dir/modules/vzw-nidd-front-end && bundle && bun install)
