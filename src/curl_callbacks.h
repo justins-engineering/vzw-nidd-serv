4da585645a4981cba3873c7aa6d91e302f5eba2b
jforgue <26842354+bandogora@users.noreply.github.com>
2024-05-24 17:33:40 -0400

    Add heap based curl write callback
    - Fine, I'll use the stupid heap
    - Prevents running out of stack mem leading to stack smashing error

--- src/curl_callbacks.h
+++ src/curl_callbacks.h
@@ -10,11 +10,20 @@ typedef struct RecvData {
   size_t size;
 } RecvData;
 
-/** @fn const size_t mem_write_callback(const char *buffer, const size_t size,
- * size_t nitems, const void *userdata);
+/** @fn const size_t stack_mem_write_callback(const char *buffer, const size_t
+ * size, size_t nitems, const void *userdata);
  *  @brief User defined curl callback for writing response data to stack mem.
  */
-size_t mem_write_callback(
+size_t stack_mem_write_callback(
+    const char *buffer, const size_t size, const size_t nitems,
+    const void *userdata
+);
+
+/** @fn const size_t heap_mem_write_callback(const char *buffer, const size_t
+ * size, size_t nitems, const void *userdata);
+ *  @brief User defined curl callback for writing response data to heap mem.
+ */
+size_t heap_mem_write_callback(
     const char *buffer, const size_t size, const size_t nitems,
     const void *userdata
 );
